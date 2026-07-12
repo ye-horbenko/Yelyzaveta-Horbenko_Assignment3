52eaad9b136688da74c1cbddeac398475fa38123
Yelyzaveta Horbenko <yelyzaveta.horbenko.25@kse.org.ua>
2026-07-12 18:52:50 +0300

    final structure task 1 with additional info

--- Task 1.py
+++ Task 1.py
@@ -1,23 +1,46 @@
-book_profile = {
-    "title": "Основи програмування",
-    "author": "Іван Петренко",
-    "year": 2023,
-    "publisher_info": {
-        "name": "Наукова думка",
-        "city": "Київ"
+book_profiles = [
+    {
+        "title": "Основи програмування",
+        "author": "Іван Петренко",
+        "year": 2023,
+        "rating": 6.0,
+        "publisher_info": {
+            "name": "Наукова думка",
+            "city": "Київ"
+        }
+    },
+    {
+        "title": "Гаррі Поттер і Келих вогню",
+        "author": "Дж. К. Роулінг",
+        "year": 2000,
+        "rating": 9.9,
+        "publisher_info": {
+            "name": "Bloomsbury",
+            "city": "Лондон"
+        }
     }
-}
+]
 
-name = book_profile["title"]
-author = book_profile["author"]
-print(f"Назва: {name}, автор: {author}")
+for book in book_profiles:
+    title = book["title"]
+    author = book["author"]
+    print(f"Назва: {title}")
+    print(f"Автор: {author}")
 
-publisher = book_profile["publisher_info"]["name"]
-print(f"Видавництво: {publisher}")
+    publisher = book["publisher_info"]["name"]
+    print(f"Видавництво: {publisher}")
 
-print(f"Книга '{name}' автора {author} була видана у місті {book_profile["publisher_info"]["city"]}")
+    city = book["publisher_info"]["city"]
+    print(f'Книга "{title}" автора {author} була видана у місті {city}.')
 
-if book_profile["year"]:
-    print(book_profile["year"])
+    if "year" in book:
+        print(f"Рік видання: {book['year']}")
+    else:
+        print("Рік видання невідомий")
+
+rating = float(input("What is your expected rating? "))
+if rating > book_profiles[0]["rating"]:
+    print(f"Perfect book for you is {book_profiles[1]["title"]}")
 else:
-    print("Рік видання невідомий")
+    print(f"You might like '{book_profiles[0]["title"]}', "
+          f"though we would still recommend '{book_profiles[1]["title"]}'")
